86adcd080ba0a8293902d09872592858cfe100f1
Alexander <abd19009@student.mdh.se>
2020-03-06 13:15:58 +0100

    neg__

--- SnakeProject/SnakeProject/GameEngine.cs
+++ SnakeProject/SnakeProject/GameEngine.cs
@@ -4,16 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace SnakeProject
 {
     class GameEngine
     {
+        int x = 50, y = 50, z = 6, k = 6;
+        MainForm window;
         public void Initialize()
         {
-            MainForm window = new MainForm();
+            window = new MainForm();
             Timer time = new Timer();
             time.Tick += TimerEventHandler;
+
             time.Interval = 1000 / 25;
             time.Start();
 
@@ -22,12 +26,19 @@ namespace SnakeProject
 
         public void Run()
         {
-
+            
         }
 
         private void TimerEventHandler(object sender, EventArgs e)
         {
-            
+
+            window.Paint += Draw;
+            window.Refresh();
+        }
+
+        public void Draw(Object obj, PaintEventArgs pe)
+        {
+            pe.Graphics.DrawEllipse(new Pen(Color.Red, 3), new Rectangle(x++, y++, z--, k));
         }
 
     }
--- SnakeProject/SnakeProject/MainForm.cs
+++ SnakeProject/SnakeProject/MainForm.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
 
 namespace SnakeProject
 {
-    class MainForm : Form1
+    class MainForm : Form
     {
         public MainForm() : base(){
             Height = 640;
             Width = 840;
+            //BackColor = Color.Purple;
             Text = "Snake Game";
             }
     }
